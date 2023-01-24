36192e8b6c90b2ecebd751d1d43164bea0c4a034
vivek1729 <1658576+vivek1729@users.noreply.github.com>
2023-01-23 19:58:08 -0800

    vp - testing github actions

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel, conlist
 
 
-
+# Initialize API server
 app = FastAPI(title="Predicting Wine Class with batching")
 
 # Open classifier in global scope
